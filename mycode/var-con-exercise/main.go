48b8e64c3e97fdf245923a474908166b6ec44e10
BicycleWalrus <tpatricksc@gmail.com>
2025-03-12 12:25:17 -0400

    Lesson 3 - Variables and Constants

---
+++ mycode/var-con-exercise/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var counter = 100
+	const appName = "GoApp"
+	isRunning := true
+
+	fmt.Println(counter)
+	fmt.Println(appName)
+	fmt.Println(isRunning)
+}
---
+++ mycode/variables/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alice"
+	fmt.Println(name)
+}
